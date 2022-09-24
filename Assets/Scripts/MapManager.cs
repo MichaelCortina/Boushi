1fb85f59f082d6a300a2ca800bcffddd13d766e3
Michael Cortina <michaelcortina30@gmail.com>
2022-09-24 00:28:12 -0400

    Created Tile Info & Map Manager

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public sealed class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map;
+
+    [SerializeField]
+    private List<TileInfo> tileInfo;
+    
+    private Dictionary<TileBase, TileInfo> _infoFromTiles;
+
+    public TileInfo InfoAtPosition(Vector2 worldPosition)
+    {
+        var cellPosition = map.WorldToCell(worldPosition);
+        var tileBase = map.GetTile(cellPosition);
+        return _infoFromTiles[tileBase];
+    }
+        
+    private void Awake() 
+    {
+        foreach (var info in tileInfo)
+            foreach (var tileBase in info.Tiles)
+                _infoFromTiles.Add(tileBase, info);
+    }
+}
---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+public class TileInfo : ScriptableObject
+{
+    [SerializeField] private TileBase[] tiles;
+    [SerializeField] private bool ground;
+
+    public TileBase[] Tiles => tiles;
+    public bool IsGround => ground;
+}
