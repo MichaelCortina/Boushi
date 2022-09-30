398f145e060dc2769fbe2c251a8224a3721fdeb7
Michael Cortina <michaelcortina30@gmail.com>
2022-09-30 10:42:14 -0400

    Moved input logic from ResetHat to InputHandler

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public sealed class InputHandler
+{
+    private readonly Dictionary<KeyCode, InputEvent> _inputEvents = new();
+
+    //Should be called every frame
+    public void HandleInput()
+    {
+        foreach (var (key, inputEvent) in _inputEvents)
+        {
+            bool hasHoldEvent = inputEvent.HoldEvent != null;
+
+            if (hasHoldEvent && inputEvent.HoldEvent.ShouldInvoke)
+            {
+                inputEvent.HoldEvent.HoldAction.Invoke();
+                inputEvent.HoldEvent.HoldTimer.Reset();
+            }
+
+            if (Input.GetKeyDown(key))
+                if (hasHoldEvent) 
+                    inputEvent.HoldEvent.HoldTimer.Start();
+                else 
+                    inputEvent.ClickEvent?.Invoke();
+            else if (Input.GetKeyUp(key) 
+                     && hasHoldEvent 
+                     && inputEvent.HoldEvent.HoldTimer.IsRunning)
+            {
+                inputEvent.ClickEvent?.Invoke();
+                inputEvent.HoldEvent.HoldTimer.Reset();
+            }
+        }
+    }
+
+    public InputHandler SetClickEvent(KeyCode key, Action clickEvent)
+    {
+        if (_inputEvents.ContainsKey(key))
+            _inputEvents[key].ClickEvent = clickEvent;
+        else
+            _inputEvents.Add(key, new InputEvent {ClickEvent = clickEvent});
+        return this;
+    }
+
+    public InputHandler SetHoldEvent(KeyCode key, float holdSeconds, Action holdEvent)
+    {
+        var hold = new HoldEvent
+        {
+            HoldSeconds = holdSeconds,
+            HoldAction = holdEvent
+        };
+
+        if (_inputEvents.ContainsKey(key))
+            _inputEvents[key].HoldEvent = hold;
+        else
+            _inputEvents.Add(key, new InputEvent { HoldEvent = hold });
+        return this;
+    }
+    
+    private sealed class InputEvent
+    {
+        public Action ClickEvent { get; set; }
+        public HoldEvent HoldEvent { get; set; }
+    }
+
+    private sealed class HoldEvent
+    {
+        public float HoldSeconds { get; set; }
+        public Action HoldAction  { get; set; }
+        public readonly Stopwatch HoldTimer = new();
+
+        public bool ShouldInvoke => HoldTimer.Elapsed.Seconds >= HoldSeconds;
+    }
+}
--- Assets/Scripts/ResetHat.cs
+++ Assets/Scripts/ResetHat.cs
@@ -4,42 +4,19 @@ using UnityEngine;
 public class ResetHat : MonoBehaviour
 {
     [SerializeField] private KeyCode resetKey;
-    //time user must hold resetKey before fully resetting scene
     [SerializeField] private float resetTime; 
     [SerializeField] private GameObject player;
 
     private Vector2 _resetPosition;
-    private Timer _resetTimer;
     private bool _hasResetPoint;
-    private bool _shouldFullReset;
+    private InputHandler _inputHandler;
     
     private SceneManager _sceneManager;
 
-    private void Update()
-    {
-        HandleInput();
-        
-        if (_shouldFullReset)
-        {
-            FullReset();
-            _shouldFullReset = false;
-        }
-    }
-
-    private void HandleInput()
-    {
-        if (Input.GetKeyDown(resetKey))
-        {
-            _resetTimer.Start();
-        }
-
-        if (Input.GetKeyUp(resetKey) && _resetTimer.Enabled)
-        {
-            SoftReset();
-            _resetTimer.Stop();
-        }
-    }
-
+    private void Update() => _inputHandler.HandleInput();
+    
+    private void FullReset() => _sceneManager.ResetAll();
+    
     private void SoftReset()
     {
         if (_hasResetPoint)
@@ -48,14 +25,12 @@ public class ResetHat : MonoBehaviour
             _resetPosition = player.transform.position;
         _hasResetPoint = !_hasResetPoint;
     }
-    
-    private void FullReset() => _sceneManager.ResetAll();
 
     private void Awake()
     {
         _sceneManager = FindObjectOfType<SceneManager>();
-        
-        _resetTimer = new Timer(resetTime * 1000);
-        _resetTimer.Elapsed += (_, _) => _shouldFullReset = true;
+        _inputHandler = new InputHandler()
+            .SetClickEvent(resetKey, SoftReset)
+            .SetHoldEvent(resetKey, resetTime, FullReset);
     }
 }
