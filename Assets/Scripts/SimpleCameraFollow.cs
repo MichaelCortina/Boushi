21ea00a8d3a294f2960ecbbc22b15662e2dcb21f
Michael Cortina <michaelcortina30@gmail.com>
2022-10-02 14:37:13 -0400

    Did some more work on camera following

--- Assets/Scripts/SimpleCameraFollow.cs
+++ Assets/Scripts/SimpleCameraFollow.cs
@@ -3,12 +3,45 @@ using UnityEngine;
 
 public class SimpleCameraFollow : MonoBehaviour
 {
-    [SerializeField] private PolygonCollider2D cameraBounds;
+    [SerializeField] private Collider2D cameraCollider;
+    [SerializeField] private Collider2D mapCollider;
     [SerializeField] private Transform player;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private KeyCode centerCamera;
 
-    private void Update()
+    private InputHandler _inputHandler;
+    private bool _centeringCamera;
+
+    private void Update() => _inputHandler.HandleInput();
+    
+    private void FixedUpdate()
     {
-        var closestPoint = cameraBounds.ClosestPoint(player.position);
-        transform.position = new Vector3(closestPoint.x, closestPoint.y, transform.position.z);
+                
+        var cameraPosition = transform.position;
+        var playerPosition = player.position;
+        
+        if (_centeringCamera)
+            cameraPosition = Vector3.MoveTowards(cameraPosition, playerPosition, cameraSpeed);
+        
+        else if (!cameraCollider.bounds.Contains(new Vector3(playerPosition.x, playerPosition.y,
+                         cameraCollider.transform.position.z)))
+            cameraPosition = Vector3.MoveTowards(
+                cameraPosition,
+                playerPosition,
+                Vector2.Distance(
+                    playerPosition,
+                    cameraCollider.ClosestPoint(playerPosition)));
+
+        cameraPosition = mapCollider.bounds.ClosestPoint(cameraPosition);
+        transform.position = new Vector3(cameraPosition.x, cameraPosition.y, transform.position.z);
+        
+        if (_centeringCamera)
+            _centeringCamera = Math.Abs(cameraPosition.x - playerPosition.x) > 0.1 || Math.Abs(cameraPosition.y - playerPosition.y) > 0.1;
     }
+    
+    private void Awake() =>
+        _inputHandler = new InputHandler()
+            .SetClickEvent(centerCamera, CenterCamera);
+
+    private void CenterCamera() => _centeringCamera = true;
 }
