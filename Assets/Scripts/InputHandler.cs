fa0bf6bc45bf6b633d3bb2ed21b008adcd072f1b
bocheung <bocheung15@gmail.com>
2022-10-05 12:58:28 -0400

    merging files

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -13,24 +13,30 @@ public sealed class InputHandler
         foreach (var (key, inputEvent) in _inputEvents)
         {
             bool hasHoldEvent = inputEvent.HoldEvent != null;
-
-            if (hasHoldEvent && inputEvent.HoldEvent.ShouldInvoke)
+            bool hasBeginHoldEvent = inputEvent.StartHoldEvent != null;
+            
+            if (hasHoldEvent && inputEvent.HasHoldTimeElapsed)
             {
                 inputEvent.HoldEvent.HoldAction.Invoke();
-                inputEvent.HoldEvent.HoldTimer.Reset();
+                inputEvent.HoldTimer.Reset();
             }
 
-            if (Input.GetKeyDown(key))
-                if (hasHoldEvent) 
-                    inputEvent.HoldEvent.HoldTimer.Start();
-                else 
-                    inputEvent.ClickEvent?.Invoke();
-            else if (Input.GetKeyUp(key) 
-                     && hasHoldEvent 
-                     && inputEvent.HoldEvent.HoldTimer.IsRunning)
+            if (inputEvent.StartHoldEvent is { HasActivated: false }
+                && inputEvent.HasDelayElapsed)
+            {
+                inputEvent.StartHoldEvent.BeginHoldAction.Invoke();
+                inputEvent.StartHoldEvent.HasActivated = true;
+            }
+
+            if (Input.GetKeyDown(key)
+                && (hasHoldEvent || hasBeginHoldEvent))
+                inputEvent.HoldTimer.Start();
+            else if (Input.GetKeyUp(key))
             {
                 inputEvent.ClickEvent?.Invoke();
-                inputEvent.HoldEvent.HoldTimer.Reset();
+                inputEvent.HoldTimer.Reset();
+                if (hasBeginHoldEvent)
+                    inputEvent.StartHoldEvent.HasActivated = false;
             }
         }
     }
@@ -58,19 +64,45 @@ public sealed class InputHandler
             _inputEvents.Add(key, new InputEvent { HoldEvent = hold });
         return this;
     }
+
+    public InputHandler SetStartHoldEvent(KeyCode key, float delay, Action beginHoldEvent)
+    {
+        var beginHold = new StartHoldEvent
+        {
+            Delay = delay,
+            BeginHoldAction = beginHoldEvent,
+        };
+
+        if (_inputEvents.ContainsKey(key))
+            _inputEvents[key].StartHoldEvent = beginHold;
+        else 
+            _inputEvents.Add(key, new InputEvent { StartHoldEvent = beginHold });
+        return this;
+    }
     
     private sealed class InputEvent
     {
         public Action ClickEvent { get; set; }
         public HoldEvent HoldEvent { get; set; }
+        public StartHoldEvent StartHoldEvent { get; set; }
+
+        public readonly Stopwatch HoldTimer = new();
+        
+        public bool HasHoldTimeElapsed => HoldTimer.Elapsed.Seconds >= HoldEvent.HoldSeconds;
+        public bool HasDelayElapsed => HoldTimer.Elapsed.Milliseconds >= StartHoldEvent.Delay;
     }
 
     private sealed class HoldEvent
     {
         public float HoldSeconds { get; set; }
         public Action HoldAction  { get; set; }
-        public readonly Stopwatch HoldTimer = new();
+    }
 
-        public bool ShouldInvoke => HoldTimer.Elapsed.Seconds >= HoldSeconds;
+    private sealed class StartHoldEvent
+    {
+        public float Delay { get; set; }
+        public bool HasActivated { get; set; }
+        public Action BeginHoldAction { get; set; }
+
     }
 }
