1d199d35f7ab1c0094b2b81edbbb22f262b6f192
bocheung <bocheung15@gmail.com>
2022-10-27 01:14:29 -0400

    Got the Text Interaction to work thanks to micheal, also added boolean check to limit the times it can be activated.

--- Assets/Scripts/TextInteraction.cs
+++ Assets/Scripts/TextInteraction.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TextInteraction : MonoBehaviour
 {
+    [SerializeField] private KeyCode interactKey = KeyCode.X;
+    [SerializeField] private UnityEvent onInteract;
+    private bool isInteracting = false;
+    private InputHandler _inputHandler;
+    private bool isPlayerInBounds;
+    
     public static Action<IEnumerable<ConversationLine>> OnTextInteraction;
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag.Equals("Player"))
+        if (other.CompareTag("Player"))
         {
-            
+            isPlayerInBounds = true;
         }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInBounds = false;
+            isInteracting = false;
+        }
+    }
+    
+    private void StartInteract()
+    {
+        if (isPlayerInBounds && !isInteracting)
+        {
+            onInteract?.Invoke();
+            isInteracting = true;
+        }
+    }
+
+    private void Update() => _inputHandler.HandleInput();
+
+    private void Awake()
+    {
+        _inputHandler = new InputHandler()
+            .SetClickEvent(interactKey, StartInteract);
     }
 }
