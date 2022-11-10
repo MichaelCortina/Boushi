d7def8f6309313844397a6ac2cd3272e4c0688d1
Michael Cortina <michaelcortina30@gmail.com>
2022-11-10 14:41:17 -0500

    switched door controller serialized fields from 3 floats to one Vector3

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -5,33 +5,23 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    [SerializeField] private float distanceInZ;
-    [SerializeField] private float distanceInX;
-    [SerializeField] private float distanceInY;
+    [SerializeField] private Vector3 openDistance;
     [SerializeField] private float timeToClose;
     [SerializeField] private float timeToOpen;
-    
-    public void OpenDoor()
-    {
-        StartCoroutine(OpenDoorCoroutine());
-    }
 
-    public void CloseDoor()
-    {
-        StartCoroutine(CloseDoorCoroutine());
-    }
+    public void OpenDoor() => StartCoroutine(OpenDoorCoroutine());
+        
+    public void CloseDoor() => StartCoroutine(CloseDoorCoroutine());
 
     private IEnumerator CloseDoorCoroutine()
     {
         yield return new WaitForSeconds(timeToClose);
-        var doorPosition = transform.position;
-        transform.position = new Vector3(doorPosition.x - distanceInX, doorPosition.y - distanceInY,  doorPosition.z - distanceInZ);
+        transform.position -= openDistance;
     }
 
     private IEnumerator OpenDoorCoroutine()
     {
         yield return new WaitForSeconds(timeToOpen);
-        var doorPosition = transform.position;
-        transform.position = new Vector3(doorPosition.x + distanceInX, doorPosition.y + distanceInY, doorPosition.z + distanceInZ);
+        transform.position += openDistance;
     }
 }
