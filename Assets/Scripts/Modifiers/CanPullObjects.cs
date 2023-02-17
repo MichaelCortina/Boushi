eafaff8a64ccc9bdc24d8cba046a8a7dc6f1911b
Michael Cortina <michaelcortina30@gmail.com>
2023-02-17 14:48:49 -0500

    fixed bug where pull only worked in certain directions

--- Assets/Scripts/Modifiers/CanPullObjects.cs
+++ Assets/Scripts/Modifiers/CanPullObjects.cs
@@ -1,89 +1,93 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CanPullObjects : MonoBehaviour
+namespace Modifiers
 {
-    [SerializeField] private KeyCode pullKey = KeyCode.LeftShift;
-    private IMovable _currentMovable;
-    
-    private Rigidbody2D _beingPulled;
-
-    private Vector2 _prevPosition;
-    private bool _isPullingObject;
-
-    /// when in contact with an object that can be pulled, if the
-    /// pullKey is pressed, begin pulling the object. When the key
-    /// is released stop pulling the object
-    private void Update()
+    public class CanPullObjects : MonoBehaviour
     {
-        if (_beingPulled is null) return;
+        [SerializeField] private KeyCode pullKey = KeyCode.LeftShift;
+        private IMovable _currentMovable;
+        private Collider2D _currentCollider;
+    
+        private Rigidbody2D _beingPulled;
 
-        if (Input.GetKeyDown(pullKey))
+        private Vector2 _prevPosition;
+        private bool _isPullingObject;
+
+        /// when in contact with an object that can be pulled, if the
+        /// pullKey is pressed, begin pulling the object. When the key
+        /// is released stop pulling the object
+        private void Update()
         {
-            _isPullingObject = true;
+            if (_beingPulled is null) return;
+
+            if (Input.GetKeyDown(pullKey))
+            {
+                _isPullingObject = true;
             
-            //when pulling slow current object based on mass of object being pulled
-            _currentMovable.ApplySpeedModifier(1/_beingPulled.mass);
+                //when pulling slow current object based on mass of object being pulled
+                _currentMovable.ApplySpeedModifier(1/_beingPulled.mass);
+            }
+
+            if (Input.GetKeyUp(pullKey))
+            {
+                // when object is released revert current object to previous speed
+                if (_isPullingObject)
+                    _currentMovable.ApplySpeedModifier(_beingPulled.mass);
+            
+                _isPullingObject = false;
+            }
         }
 
-        if (Input.GetKeyUp(pullKey))
+        /// when the current object comes in contact with another object that can
+        /// be pulled, store a reference to the object unless another has already
+        /// been stored.
+        private void OnCollisionEnter2D(Collision2D collision)
         {
+            var movable = collision.gameObject.GetComponent<IMovable>();
+
+            if (movable is null || _beingPulled is not null) return;
+
+            _beingPulled = collision.rigidbody;
+        }
+    
+        /// when the current object loses contact with the object being pulled,
+        /// stop pulling it.
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (_beingPulled is null || collision.rigidbody != _beingPulled) return;
+        
             // when object is released revert current object to previous speed
             if (_isPullingObject)
                 _currentMovable.ApplySpeedModifier(_beingPulled.mass);
-            
+        
+            _beingPulled = null;
             _isPullingObject = false;
         }
-    }
 
-    /// when the current object comes in contact with another object that can
-    /// be pulled, store a reference to the object unless another has already
-    /// been stored.
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        var movable = collision.gameObject.GetComponent<IMovable>();
-
-        if (movable is null || _beingPulled is not null) return;
-
-        _beingPulled = collision.rigidbody;
-    }
-    
-    /// when the current object loses contact with the object being pulled,
-    /// stop pulling it.
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (_beingPulled is null || collision.rigidbody != _beingPulled) return;
-        
-        // when object is released revert current object to previous speed
-        if (_isPullingObject)
-            _currentMovable.ApplySpeedModifier(_beingPulled.mass);
-        
-        _beingPulled = null;
-        _isPullingObject = false;
-    }
-
-    /// if an object is being pulled, update its position to follow the
-    /// player's movements
-    private void FixedUpdate()
-    {
-        Vector2 currentPosition = transform.position;
-        
-        if (_isPullingObject)
+        /// if an object is being pulled, update its position to follow the
+        /// player's movements
+        private void FixedUpdate()
         {
-            Vector2 changeInPosition = currentPosition - _prevPosition;
-            Vector2 prevClosestPoint = _beingPulled.ClosestPoint(_prevPosition);
-            Vector2 currentClosestPoint = _beingPulled.ClosestPoint(currentPosition);
+            Vector2 currentPosition = _currentCollider.bounds.center;
+        
+            if (_isPullingObject)
+            {
+                Vector2 changeInPosition = currentPosition - _prevPosition;
+                Vector2 prevClosestPoint = _beingPulled.ClosestPoint(_prevPosition);
+                Vector2 currentClosestPoint = _beingPulled.ClosestPoint(currentPosition);
 
-            // do not allow object to be pulled side to side
-            if (Vector2.Distance(prevClosestPoint, currentClosestPoint) == 0)
-                _beingPulled.position += changeInPosition;
+                // do not allow object to be pulled side to side
+                if (Vector2.Distance(prevClosestPoint, currentClosestPoint) == 0)
+                    _beingPulled.position += changeInPosition;
+            }
+
+            _prevPosition = currentPosition;
         }
 
-        _prevPosition = currentPosition;
-    }
-
-    private void Awake()
-    {
-        _currentMovable = GetComponent<IMovable>();
+        private void Awake()
+        {
+            _currentMovable = GetComponent<IMovable>();
+            _currentCollider = GetComponent<Collider2D>();
+        }
     }
 }
