ca9a7524fef239d149808026e19b77169895daee
Michael Cortina <michaelcortina30@gmail.com>
2023-02-23 13:26:28 -0500

    completed the tutorial bridge puzzle

---
+++ Assets/Scripts/BridgeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// transforms a rectangular piece of tile from on to off whenever change states
+/// is called starting at position, and extending upwards for height tiles as
+/// well as extending to the right, length tiles.
+public class BridgeController : MonoBehaviour
+{
+    [SerializeField] private bool isActivated = false;
+    [SerializeField] private Tile on;
+    [SerializeField] private Tile off;
+    [SerializeField] private Tilemap map;
+    
+    [Header("Bridge Dimensions")]
+    [SerializeField] private Vector3Int position;
+    [SerializeField] private int length;
+    [SerializeField] private int height;
+
+    //transforms tiles in the range from on to off and vice versa
+    public void ChangeStates() =>
+        map.BoxFill(position, !(isActivated = !isActivated) ? off : on, 
+            position.x, position.y, position.x + length, position.y + height);
+}
--- Assets/Scripts/ControlSwitch.cs
+++ Assets/Scripts/ControlSwitch.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+/// Invokes the UnityEvent onActivate when the switch has been activated
+/// requiredActivation times. When deactivate is called the current activations
+/// decreases by 1, if this would bring the current activations to below the required
+/// activations the onExit UnityEvent is invoked.
 public class ControlSwitch : MonoBehaviour
 {
     [SerializeField] private int currentActivations;
@@ -8,17 +12,19 @@ public class ControlSwitch : MonoBehaviour
     [SerializeField] private UnityEvent onActivate;
     [SerializeField] private UnityEvent onExit;
 
+    /// increments currentActivations by 1, and invokes onActivate
+    /// if currentActivations is equal to required activations
     public void Activate()
     {
-        currentActivations++;
-        if (currentActivations == requiredActivations)
+        if (++currentActivations == requiredActivations)
             onActivate?.Invoke();
     }
 
+    /// decrements currentActivations by one, and invokes onExit
+    /// if currentActivations has decreased to below requiredActivations
     public void Deactivate()
     {
-        currentActivations--;
-        if (currentActivations == requiredActivations - 1)
+        if (currentActivations-- == requiredActivations)
             onExit?.Invoke();
     }
 }
