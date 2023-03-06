2df45d91886937dd216f2463a9d7d1493ce3a923
bocheung <bocheung15@gmail.com>
2023-03-06 00:44:37 -0500

    Micheal help Im tryna make it so movable objects can only move on an axis and idk y code no worky

--- Assets/Scripts/Modifiers/Movable/MovableObject.cs
+++ Assets/Scripts/Modifiers/Movable/MovableObject.cs
@@ -6,7 +6,8 @@ class MovableObject : MonoBehaviour, IMovable
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float moveSpeedModifier = 1f;
-    
+    [SerializeField] private Boolean canMoveXAxis = true;
+    [SerializeField] private Boolean canMoveYAxis = true;
     public event EventHandler<ObjectMovedEventArgs> OnObjectMoved;
     
     private Rigidbody2D _rb;
@@ -24,7 +25,20 @@ class MovableObject : MonoBehaviour, IMovable
 
     private void FixedUpdate()
     {
-        _rb.MovePosition(_rb.position + _moveDirection * (ModifiedSpeed * Time.fixedDeltaTime));
+        
+        if (canMoveXAxis && canMoveYAxis)
+        {
+            _rb.MovePosition(_rb.position + _moveDirection * (ModifiedSpeed * Time.fixedDeltaTime));
+        }else if (canMoveXAxis)
+        {
+            Vector2 change = new Vector2(_rb.position.x + _moveDirection.x,_rb.position.y);
+            _rb.MovePosition(change * (ModifiedSpeed * Time.fixedDeltaTime));
+        }else if (canMoveYAxis)
+        {
+            Vector2 change = new Vector2(_rb.position.x,_rb.position.y + _moveDirection.y);
+            _rb.MovePosition(change * (ModifiedSpeed * Time.fixedDeltaTime));
+        }
+        
         if (transform.position != _previousPosition)
             OnObjectMoved?.Invoke(this, new ObjectMovedEventArgs(_col.bounds));
         _previousPosition = transform.position;
