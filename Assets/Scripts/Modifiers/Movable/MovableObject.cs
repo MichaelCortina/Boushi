ee6c8786155ce22ce85997cd41d23ba52883282d
Michael Cortina <michaelcortina30@gmail.com>
2023-03-06 20:06:44 -0500

    fixed what Bo was working on

--- Assets/Scripts/Modifiers/Movable/MovableObject.cs
+++ Assets/Scripts/Modifiers/Movable/MovableObject.cs
@@ -6,8 +6,8 @@ class MovableObject : MonoBehaviour, IMovable
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float moveSpeedModifier = 1f;
-    [SerializeField] private Boolean canMoveXAxis = true;
-    [SerializeField] private Boolean canMoveYAxis = true;
+    [SerializeField] private bool canMoveXAxis = true;
+    [SerializeField] private bool canMoveYAxis = true;
     public event EventHandler<ObjectMovedEventArgs> OnObjectMoved;
     
     private Rigidbody2D _rb;
@@ -21,24 +21,15 @@ class MovableObject : MonoBehaviour, IMovable
 
     public void ApplySpeedModifier(float speedPercentage) => moveSpeedModifier *= speedPercentage;
     
-    public void MoveObject(Vector2 direction) => _moveDirection = direction;
+    public void MoveObject(Vector2 direction) => 
+        _moveDirection = new(
+            direction.x * Convert.ToInt32(canMoveXAxis), 
+            direction.y * Convert.ToInt32(canMoveYAxis));
 
     private void FixedUpdate()
     {
-        
-        if (canMoveXAxis && canMoveYAxis)
-        {
-            _rb.MovePosition(_rb.position + _moveDirection * (ModifiedSpeed * Time.fixedDeltaTime));
-        }else if (canMoveXAxis)
-        {
-            Vector2 change = new Vector2(_rb.position.x + _moveDirection.x,_rb.position.y);
-            _rb.MovePosition(change * (ModifiedSpeed * Time.fixedDeltaTime));
-        }else if (canMoveYAxis)
-        {
-            Vector2 change = new Vector2(_rb.position.x,_rb.position.y + _moveDirection.y);
-            _rb.MovePosition(change * (ModifiedSpeed * Time.fixedDeltaTime));
-        }
-        
+        _rb.MovePosition(_rb.position + _moveDirection * (ModifiedSpeed * Time.fixedDeltaTime));
+
         if (transform.position != _previousPosition)
             OnObjectMoved?.Invoke(this, new ObjectMovedEventArgs(_col.bounds));
         _previousPosition = transform.position;
