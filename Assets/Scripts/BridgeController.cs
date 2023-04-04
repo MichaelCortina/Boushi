951f5a21d3ee19c1582ffa9fd0197f479bc05677
Michael Cortina <michaelcortina30@gmail.com>
2023-04-03 21:03:20 -0400

    added OnValidate to BridgeController.cs, updating state with isActivated accordingly

--- Assets/Scripts/BridgeController.cs
+++ Assets/Scripts/BridgeController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 /// transforms a rectangular piece of tile from on to off whenever change states
@@ -7,6 +8,8 @@ using UnityEngine.Tilemaps;
 public class BridgeController : MonoBehaviour
 {
     [SerializeField] private bool isActivated = false;
+    private bool _backingActivated;
+    
     [SerializeField] private Tile on;
     [SerializeField] private Tile off;
     [SerializeField] private Tilemap map;
@@ -15,9 +18,23 @@ public class BridgeController : MonoBehaviour
     [SerializeField] private Vector3Int position;
     [SerializeField] private int length;
     [SerializeField] private int height;
+    
 
     //transforms tiles in the range from on to off and vice versa
-    public void ChangeStates() =>
-        map.BoxFill(position, !(isActivated = !isActivated) ? off : on, 
+    public void ChangeStates()
+    {
+        map.BoxFill(position, !(isActivated = !isActivated) ? off : on,
             position.x, position.y, position.x + length, position.y + height);
+
+        _backingActivated = isActivated;
+    }
+
+    private void OnValidate()
+    {
+        if (isActivated != _backingActivated)
+        {
+            isActivated = _backingActivated;
+            ChangeStates();
+        }
+    }
 }
