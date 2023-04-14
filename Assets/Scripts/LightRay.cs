86c443e3bd43e7e747b3110c0f7472d939043666
Michael Cortina <michaelcortina30@gmail.com>
2023-04-14 16:34:41 -0400

    implemented mirrors for the water temple

--- Assets/Scripts/LightRay.cs
+++ Assets/Scripts/LightRay.cs
@@ -1,27 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class LightRay : MonoBehaviour
 {
+    [SerializeField] private float lightOffset = 0.02f;
+    
     private LineRenderer _lineRenderer;
     private int _index;
-    
+
     private void FixedUpdate()
     {
         _index = 1;
-        Vector2 origin = _lineRenderer.GetPosition(0);
+        Vector3 origin = _lineRenderer.GetPosition(0);
+        
+        //reset position of the ray each frame
+        for (int i = 0; i < _lineRenderer.positionCount; i++)
+            _lineRenderer.SetPosition(0, origin);
 
         CastRay(origin, Vector2.up);
     }
 
     public void CastRay(Vector2 origin, Vector2 direction)
     {
+        if (_index >= _lineRenderer.positionCount)
+            return;
+        
         RaycastHit2D rayHit = Physics2D.Raycast(origin, direction);
         var rayController = rayHit.collider?.GetComponent<IRayController>();
-
-        _lineRenderer.SetPosition(_index, rayHit.point.Extend(-0.1f));
-        _index++;
         
+        // create small offset so a reflection does not hit the same contact point again
+        Vector2 offset = rayHit.point * direction * lightOffset;
+        rayHit.point += offset;
+        
+        //set each point in the lineRenderer past index to the target position (rayHit)
+        for (int i = _index++; i < _lineRenderer.positionCount; i++)
+            _lineRenderer.SetPosition(i, rayHit.point.Extend(-0.1f));
+
         rayController?.OnRayHit(this, rayHit.point, direction);
     }
 
---
+++ Assets/Scripts/Mirror.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Mirror : MonoBehaviour, IRayController
+{
+    [SerializeField] [Range(0f, 360f)] private float angle;
+
+    public void OnRayHit(LightRay ray, Vector2 contactPoint, Vector2 direction)
+    {
+        // multiply the direction of the ray by the rotation matrix with theta = angle in radians
+        float radians = (angle * Mathf.PI) / 180;
+        float x = direction.x * Mathf.Cos(radians) - direction.y * Mathf.Sin(radians);
+        float y = direction.x * Mathf.Sin(radians) + direction.y * Mathf.Cos(radians);
+
+        // then change the direction to the new rotated direction and cast the ray again
+        direction = new Vector2(x, y);
+
+        ray.CastRay(contactPoint, direction);
+    }
+}
