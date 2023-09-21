4e0688b278f0bc2bc688f7568c16122dc368579f
Michael Cortina <michaelcortina30@gmail.com>
2023-09-20 21:19:53 -0400

    added Bag data structure for possible use in inventory

---
+++ Assets/Scripts/InventorySystem/Bag.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Bag<T> : IEnumerable
+{
+    private Dictionary<T, int> _dict;
+    
+    public int Count { get; private set; }
+
+    public Bag()
+    {
+        _dict = new();
+    }
+
+    public Bag(IEnumerable<T> iter)
+    {
+        foreach (var item in iter)
+        {
+            Add(item);
+        }
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var (item, num) in _dict)
+        {
+            for (int i = 0; i <= num; i++)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T item)
+    {
+        if (item is null) return;
+
+        if (_dict.ContainsKey(item))
+        {
+            _dict[item]++;
+        }
+        else
+        {
+            _dict.Add(item, 1);
+        }
+
+        Count++;
+    }
+
+    public void Clear()
+    {
+        _dict.Clear();
+        Count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        return _dict.ContainsKey(item);
+    }
+    
+    public bool Remove(T item)
+    {
+        if (_dict.ContainsKey(item))
+        {
+            if (_dict[item] > 0)
+            {
+                _dict[item]--;
+            }
+            else
+            {
+                _dict.Remove(item);
+            }
+
+            Count--;
+            
+            return true;
+        }
+
+        return false;
+    }
+}
