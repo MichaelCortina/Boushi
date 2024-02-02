c4babac31bd82fe0392db617506e55cdce4dee43
Michael Cortina <michaelcortina30@gmail.com>
2024-02-01 20:54:23 -0500

    updated bag to implement ICollection<>, cleaned up Inventory, added key to SampleScene

--- Assets/Scripts/InventorySystem/InventoryData.cs
+++ Assets/Scripts/InventorySystem/InventoryData.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace InventorySystem
 {
     [CreateAssetMenu]
     internal class InventoryData : ScriptableObject
     {
-        public Bag<ItemInstance> Bag;
+        // public Bag<ItemInstance> bag;
+        public List<ItemInstance> bag; // so that it displays in Unity
     }
 }
--- Assets/Scripts/InventorySystem/ItemInstance.cs
+++ Assets/Scripts/InventorySystem/ItemInstance.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace InventorySystem
 {
+    [Serializable]
     public class ItemInstance
     {
         public ItemData itemType;
