75cbd6617081c2a453896becfe4cf1567fc6dd05
bocheung <bocheung15@gmail.com>
2024-02-01 21:09:00 -0500

    Mirror stuff

--- Assets/Scripts/Puzzle Interactables/RayTarget.cs
+++ Assets/Scripts/Puzzle Interactables/RayTarget.cs
@@ -15,15 +15,28 @@ public class RayTarget : MonoBehaviour, IRayController
 
     public void OnRayHit(LightRay ray, Vector2 contactPoint, Vector2 direction)
     {
-        if (_triggered) return;
+        //If already event activated return
+        if (_triggered)
+            return;
         
+        //Else add new rays to set
         _rayHits.Add(ray);
+        print("Hit" + _rayHits.Count);
+
+        //Once required hits is met activate event
         if (_rayHits.Count >= requiredHits)
+        {
+            //mark as triggered
+            _triggered = true;
             onTrigger.Invoke();
+        }
     }
 
     private void FixedUpdate()
     {
-        _rayHits.Clear();
+        if (!_triggered)
+        {
+            _rayHits.Clear();
+        }
     }
 }
