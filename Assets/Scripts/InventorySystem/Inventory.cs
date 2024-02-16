a82407e87117ec98f73680a04fcc2a9556332505
Michael Cortina <michaelcortina30@gmail.com>
2024-02-15 21:02:27 -0500

    created key and lock system

--- Assets/Scripts/InventorySystem/Inventory.cs
+++ Assets/Scripts/InventorySystem/Inventory.cs
@@ -1,28 +1,36 @@
-using System;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 namespace InventorySystem
 {
     public class Inventory : MonoBehaviour
         {
             [SerializeField] private InventoryData inventoryData;
-            [SerializeField] private bool saveItems = false;
+            [SerializeField] private bool saveItems;
 
-            private ICollection<ItemInstance> _items;
+            private ICollection<ItemData> _items;
             
-            public void AddItem(ItemInstance itemToAdd)
-            { 
-                _items.Add(itemToAdd);
+            /// <summary>
+            /// adds item <c>itemToAdd</c> to the inventory
+            /// </summary>
+            /// <param name="itemToAdd">non null ItemInstance</param>
+            public void AddItem([NotNull] ItemInstance itemToAdd)
+            {
+                Debug.Assert(itemToAdd != null, nameof(itemToAdd) + " != null");
+                
+                _items.Add(itemToAdd.ItemType);
             }
-            public void RemoveItem(ItemInstance itemToRemove)
-            { 
-                _items.Remove(itemToRemove);
+
+            public bool ContainsItem(ItemData item)
+            {
+                return _items.Contains(item);
             }
 
             private void Awake()
             {
-                _items = saveItems ? inventoryData.bag : new List<ItemInstance>();
+                _items = saveItems ? inventoryData.bag : new List<ItemData>();
             }
         }
 }
---
+++ Assets/Scripts/Modifiers/Interactable/Lock.cs
@@ -0,0 +1,24 @@
+﻿using InventorySystem;
+using UnityEngine;
+using UnityEngine.Events;
+using Utilities;
+
+namespace Modifiers.Interactable
+{
+    public class Lock : MonoBehaviour
+    {
+        [SerializeField] private ItemData key;
+        [SerializeField] private UnityEvent onUnlock;
+        
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.CompareTag("Player")
+                && Input.GetKey(Keybindings.Instance.InteractKey)
+                && other.GetComponent<Inventory>().ContainsItem(key))
+            {
+                onUnlock.Invoke();
+                Destroy(this);
+            }
+        }
+    }
+}
