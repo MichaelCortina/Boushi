c293908b67d48738bb80dba7a1ef7f5cdcbb0ad9
ellisonhsc <ehschiang@gmail.com>
2024-06-06 00:11:49 -0400

    tried to add motion animations

--- Assets/Scripts/Modifiers/PlayerModifiers/PlayerInput.cs
+++ Assets/Scripts/Modifiers/PlayerModifiers/PlayerInput.cs
@@ -8,6 +8,7 @@ public class PlayerInput : MonoBehaviour
 {
     private IMovable _movable;
     //private InventorySystem inventory;
+    private Animator animator;
 
     private void Update()
     {
@@ -19,5 +20,12 @@ public class PlayerInput : MonoBehaviour
     {
         _movable = GetComponent<IMovable>();
         //inventory = new InventorySystem();
+        animator = GetComponent<Animator>();
+    }
+
+    void onMove()
+    {
+        animator.SetFloat("x_move",Input.GetAxis("Horizontal"));
+        animator.SetFloat("y_move",Input.GetAxis("Vertical"));
     }
 }
